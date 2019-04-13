25905cb87cfd82bdf51aecac74b6b6d54f941754
LittleDove <757459307@qq.com>
2019-04-13 12:30:27 +0800

    add topo script code

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # mininet-OS3E
+
+The Topology.py and geo.py are from project FSPEngine https://github.com/spirosmastorakis/FSP_Engine
--- topo_script.py
+++ topo_script.py
@@ -1,58 +1,33 @@
 import re, json
 import networkx as nx
+import Topology
 from mininet.topo import Topo
 
 INFO_FILE_PATH = "os3e_latlong.json"
+METERS_TO_MILES = 0.000621371192
 
-# copied from
-def OS3EGraph():
-    g = nx.Graph()
-    g.add_path(["Vancouver", "Seattle"])
-    g.add_path(["Seattle", "Missoula", "Minneapolis", "Chicago"])
-    g.add_path(["Seattle", "SaltLakeCity"])
-    g.add_path(["Seattle", "Portland", "Sunnyvale"])
-    g.add_path(["Sunnyvale", "SaltLakeCity"])
-    g.add_path(["Sunnyvale", "LosAngeles"])
-    g.add_path(["LosAngeles", "SaltLakeCity"])
-    g.add_path(["LosAngeles", "Tucson", "ElPaso"])
-    g.add_path(["SaltLakeCity", "Denver"])
-    g.add_path(["Denver", "Albuquerque", "ElPaso"])
-    g.add_path(["Denver", "KansasCity", "Chicago"])
-    g.add_path(["KansasCity", "Dallas", "Houston"])
-    g.add_path(["ElPaso", "Houston"])
-    g.add_path(["Houston", "Jackson", "Memphis", "Nashville"])
-    g.add_path(["Houston", "BatonRouge", "Jacksonville"])
-    g.add_path(["Chicago", "Indianapolis", "Louisville", "Nashville"])
-    g.add_path(["Nashville", "Atlanta"])
-    g.add_path(["Atlanta", "Jacksonville"])
-    g.add_path(["Jacksonville", "Miami"])
-    g.add_path(["Chicago", "Cleveland"])
-    g.add_path(["Cleveland", "Buffalo", "Boston", "NewYork", "Philadelphia", "Washington"])
-    g.add_path(["Cleveland", "Pittsburgh", "Ashburn", "Washington"])
-    g.add_path(["Washington", "Raleigh", "Atlanta"])
-    return g
 
-def get_latency_info():
-    info = None
-    data_str = ""
-    with open(INFO_FILE_PATH) as f:
-        lines = f.readlines()
-        for line in lines:
-            data_str += line
-    data_dict = json.loads(data_str)
-    return info
+def calc_latency(weight):
+    # the weight of OS3EWeightedGraph is the path length in miles
+    # return delay in milliseconds ( using light speed )
+    return weight / METERS_TO_MILES / (3 * 1e8) * 1000
+
 
 class OS3ETopo(Topo):
-
     def __init__(self):
         Topo.__init__(self)
         self.switches = {}
-        g = OS3EGraph()
+        self.hosts = {}
+        g = Topology.OS3EWeightedGraph()
         for node in g.nodes:
             self.switches[node] = self.addSwitch(node)
         for edge in g.edges:
-            info = get_latency_info()
-            if info is None:
-                self.addLink(self.switches[edge[0]], self.switches[edge[1]])
+            weight = g[edge[0]][edge[1]]['weight']
+            delay = calc_latency(weight)
+            if weight is not None:
+                self.addLink(self.switches[edge[0]], self.switches[edge[1]], delay=str(delay)+'ms')
+        for node in g.nodes:
+            self.hosts[node] = self.addHost(node+'-host')
+            self.addLink(self.switches[node], self.hosts[node])
 
 topos = {'mytopo': (lambda: OS3ETopo())}
